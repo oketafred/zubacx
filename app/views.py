bca9b84b98726a9d9d18e5102abcef7acca930bb
JSnakegitHub <nyekowalter69@gmail.com>
2019-02-15 15:58:23 +0300

    fix-email-alerts

--- app/views.py
+++ app/views.py
@@ -182,7 +182,9 @@ def add_ticket():
     theEngineers = ticketInstance.get_engineers()
     theWorkOrderTypes = ticketInstance.get_work_order_types()
     users = usersInstance.users_who_can_receive_email()
-    recipients = [users]
+    recipients = [users[0][0]]
+    print(users[0][0])
+    print(recipients)
     body="""A ticket has just been opened and assigend to engineer {}, 
 the reason for this ticket is to address the issue of {}. You are receiving this notification because 
 your account with Zubacx call-center system is configured to receive these alerts.""".format(ticket_assigned_to,ticket_reason)
